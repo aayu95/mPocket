7fe02f3d145efb32e3113607107b734a25a5e70e
Abhishek Raina <araina97@gmail.com>
2015-07-06 19:19:12 +0530

    Added Digital font

--- app/src/main/java/mpocket/project/com/mpocket/MyWallet.java
+++ app/src/main/java/mpocket/project/com/mpocket/MyWallet.java
@@ -1,18 +1,27 @@
 package mpocket.project.com.mpocket;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 /**
  * Created by abhishek on 6/7/15.
  */
 public class MyWallet extends ActionBarActivity {
+
+    TextView balance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_wallet);
 
         getSupportActionBar().setTitle("myWallet");
+
+        balance = (TextView) findViewById(R.id.balance);
+
+       // Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/digital-7 (italic).ttf");
+       // balance.setTypeface(tf);
 
     }
 }
