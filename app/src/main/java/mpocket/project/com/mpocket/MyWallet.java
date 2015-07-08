bdf437f78719e0a226475d8e69395b8824470a07
Abhishek Raina <araina97@gmail.com>
2015-07-08 16:27:53 +0530

    Added Input Dialog with Amount Button

--- app/src/main/java/mpocket/project/com/mpocket/MyWallet.java
+++ app/src/main/java/mpocket/project/com/mpocket/MyWallet.java
@@ -1,14 +1,26 @@
 package mpocket.project.com.mpocket;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 /**
  * Created by abhishek on 6/7/15.
  */
 public class MyWallet extends ActionBarActivity {
+
+    Context context = this;
 
     TextView balance;
     @Override
@@ -23,5 +35,35 @@ public class MyWallet extends ActionBarActivity {
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/digital-7 (mono).ttf");
         balance.setTypeface(tf);
 
+    }
+    /*
+    *  Starts Dialog Activity
+    * */
+    public void showDialog(View view) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(R.layout.amount_dialog, null);
+
+        final AlertDialog.Builder customDialog = new AlertDialog.Builder(context);
+
+        customDialog.setView(dialogView);
+
+        EditText userInput = (EditText) dialogView
+                .findViewById(R.id.amountTextview);
+
+        customDialog.setCancelable(true);
+        customDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Amount Added Succesfully" ,Toast.LENGTH_SHORT).show();
+            }
+        });
+        customDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        customDialog.create().show();
     }
 }
