8bd13f445e860ee455ebdb8453148b0f5f869aa6
Abhishek Raina <araina97@gmail.com>
2015-07-09 18:18:51 +0530

    Added Datepicker Dialog in myExpenses Activity

--- app/src/main/java/mpocket/project/com/mpocket/PersonalExpenses.java
+++ app/src/main/java/mpocket/project/com/mpocket/PersonalExpenses.java
@@ -1,19 +1,86 @@
 package mpocket.project.com.mpocket;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by abhishek on 8/7/15.
  */
 public class PersonalExpenses extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_expenses);
+        getSupportActionBar().setTitle("myExpenses");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.personal_expenses, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_calender) {
+            startCalenderDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startCalenderDialog() {
+        Context context = this;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View dialogView = inflater.inflate(R.layout.calender_dialog, null);
+
+        final AlertDialog.Builder customCalenderDialog = new AlertDialog.Builder(context);
+
+
+
+        customCalenderDialog.setView(dialogView);
+
+        customCalenderDialog.setCancelable(true);
+        customCalenderDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        customCalenderDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        customCalenderDialog.create().show();
 
     }
+
+
 }
